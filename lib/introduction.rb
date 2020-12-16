585771429e2bdef649538a7a0acb5468a0c3e2fe
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-12-16 01:42:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,7 +13,7 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language("Jahaira", "Ruby")
+introduction_with_language("Jahaira", "Java")
 
 def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
